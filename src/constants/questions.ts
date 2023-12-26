2fa06e48e8702eafa0c73cedd22d04d19217c9ed
Zena <33304871+Heojiyeon@users.noreply.github.com>
2023-12-26 16:52:18 +0900

    feat: 동물 찾기 게임 페이지 레이아웃 구현

---
+++ src/constants/questions.ts
@@ -0,0 +1,27 @@
+export const Questions = [
+  {
+    order: 1,
+    title: '고양이',
+    answer: 'cat',
+  },
+  {
+    order: 2,
+    title: '강아지',
+    answer: 'dog',
+  },
+  {
+    order: 3,
+    title: '문어',
+    answer: 'octopus',
+  },
+  {
+    order: 4,
+    title: '토끼',
+    answer: 'rabbit',
+  },
+  {
+    order: 5,
+    title: '다람쥐',
+    answer: 'squirrel',
+  },
+];
--- src/pages/findAnimals.ts
+++ src/pages/findAnimals.ts
@@ -1,13 +1,49 @@
+import { fabric } from 'fabric';
+import { Questions } from '../constants/questions';
+
 type FindAnimalsPageProp = {
   $app: HTMLElement | null;
 };
 
 export default function FindAnimalsPage({ $app }: FindAnimalsPageProp) {
-  const findAnimalContent = document.createElement('div');
-  findAnimalContent.innerHTML = '<div>동물 찾기 게임 페이지</div>';
+  let currentOrder = 0;
+
+  const $questionTitle = document.createElement('div');
+  const $questionContent = document.createElement('canvas');
+  const $questionRemaining = document.createElement('div');
+
+  $questionContent.setAttribute('width', '600');
+  $questionContent.setAttribute('height', '500');
+
+  const canvas = new fabric.Canvas($questionContent);
+
+  /**
+   * canvas image 생성
+   */
+  Questions.map(question => {
+    fabric.Image.fromURL(
+      `../../public/images/${question.answer}.png`,
+
+      function (img) {
+        const image = img
+          .set({ left: question.order * 100, top: 200 })
+          .scale(0.15);
+
+        canvas.add(image).renderAll();
+      },
+      { crossOrigin: 'anonymous' }
+    );
+  });
+
+  $questionTitle.innerText = `${currentOrder + 1}. ${
+    Questions[currentOrder].title
+  }를 찾아주세요.`;
+  $questionRemaining.innerText = `남은 문제 수 : ${9 - currentOrder}`;
 
   const render = () => {
-    $app?.appendChild(findAnimalContent);
+    $app?.appendChild($questionTitle);
+    $app?.appendChild($questionContent);
+    $app?.appendChild($questionRemaining);
   };
 
   render();
